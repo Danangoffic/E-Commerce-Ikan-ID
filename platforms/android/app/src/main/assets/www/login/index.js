a2904da6a57aad83ce661274a1a0b9f76ce5e927
Ichel Lalusu <isel.lalusu@gmail.com>
2020-08-21 15:58:20 +0700

    loadkeranjang

--- platforms/android/app/src/main/assets/www/login/index.js
+++ platforms/android/app/src/main/assets/www/login/index.js
@@ -53,8 +53,9 @@ function kirim_data() {
                     localStorage.setItem("id_usaha", resp.data_usaha.id_usaha);
                     localStorage.setItem("data_usaha", JSON.stringify(resp.data_usaha));
                 }
-                var keranjang = JSON.stringify(Array());
-                window.localStorage.setItem('keranjang', JSON.parse(keranjang));
+                if(localStorage.usergroup == "pembeli"){
+                    loadkeranjang();
+                }
                 console.log(window.localStorage.getItem(usergroup));
                 console.log(window.localStorage.getItem(username));
                 setTimeout(redirectPage, 1000);
@@ -74,3 +75,14 @@ function redirectPage() {
     }
 }
 console.log('sukses_login' + window.localStorage.getItem('sukses_login'));
+
+function loadkeranjang() {
+    $.getJSON(base_url+"keranjang",{id_akun: localStorage.id_akun}).then(successKeranjang);
+    var successKeranjang = function(response,status){
+        if(status == "success"){
+            let keranjang = JSON.stringify(response.keranjang);
+            localStorage.setItem("keranjang", keranjang);
+            $("#status_keranjang").html(JSON.parse(keranjang).length);
+        }
+    }
+}
